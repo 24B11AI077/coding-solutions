195bb241ed3c1a1136e37679af22348958679eda
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-07-29 09:30:43 +0530

    ✅ Boats to Save People | Medium | C++ | Runtime: 42 ms (5.88%) | Memory: 50.9 MB | 2026-07-29

--- leetcode/medium/0881-boats-to-save-people/solution.cpp
+++ leetcode/medium/0881-boats-to-save-people/solution.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(),people.end());
-        int i= 0, j = people.size()-1;
-        int boats = 0;
-        while(i < j){
-            if((people[i]+people[j]) <= limit){
-                boats++;
-                i++;j--;
-            }
-            else {
-                boats++;
-                j--;
-            }
-        }
-        if(i == j){
-            boats++;
-        }
-        return boats;
+       int numRescueBoats(vector<int> people, int limit) {
+        int i, j;
+        sort(people.rbegin(), people.rend());
+        for (i = 0, j = people.size() - 1; i <= j; ++i)
+            if (people[i] + people[j] <= limit) j--;
+        return i;
     }
 };
