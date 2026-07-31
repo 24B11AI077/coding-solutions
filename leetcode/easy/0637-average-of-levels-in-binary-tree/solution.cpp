977625614af092e7216b526cf6c2d7e6e773c8ee
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-07-31 14:52:33 +0530

    ✅ Average of Levels in Binary Tree | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 23.9 MB | 2026-07-31

---
+++ leetcode/easy/0637-average-of-levels-in-binary-tree/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        queue<TreeNode*> bfs;
+        bfs.push(root);
+        vector<double> res;
+        while(!bfs.empty()){
+            int size = bfs.size();
+            double sum = 0;
+            for(int i = 0; i < size; i++){
+                TreeNode* node = bfs.front();
+                sum += node->val;
+                bfs.pop();
+
+                if(node->left){
+                    bfs.push(node->left);
+                }
+                if(node->right) bfs.push(node->right);
+            }
+            double avg = sum / size;
+            res.push_back(avg);
+        }
+        return res;
+
+    }
+};
