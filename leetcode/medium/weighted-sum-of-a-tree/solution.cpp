85796244567219c1fa18bc97fd7bc268b1202567
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-08-09 08:59:25 +0530

    ✅ Q2. Weighted Sum of a Tree | Medium | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-08-09

--- leetcode/medium/weighted-sum-of-a-tree/solution.cpp
+++ leetcode/medium/weighted-sum-of-a-tree/solution.cpp
@@ -5,7 +5,7 @@ public:
         int  n = parent.size();
         vector<int> depth(n);
         depth[0] = 1;
-       int maxi = 0;
+       int maxi = 1;
         for(int i = 1; i < n ; i++){
            depth[i] = depth[parent[i]]+1;
             maxi = max(maxi,depth[i]);
