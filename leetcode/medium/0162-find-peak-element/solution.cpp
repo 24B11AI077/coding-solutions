c438b628ee31e9b72d947af461f3403019054957
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-08-11 10:35:00 +0530

    ✅ Find Peak Element | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 12.7 MB | 2026-08-11

---
+++ leetcode/medium/0162-find-peak-element/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+       int low = 0, high = nums.size()-1;
+       while(low < high){
+        int mid = low + (high - low) / 2;
+        if(mid-1 >= 0 && (mid+1) < nums.size() && nums[mid] > nums[mid+1] && nums[mid] >nums[mid-1]){
+            return mid;
+        }
+        else if(mid-1 < 0 && (mid+1) < nums.size() && nums[mid] > nums[mid+1] ){
+            return mid;
+        }
+        else if(mid-1 >= 0 && (mid+1) >= nums.size() && nums[mid] >nums[mid-1]){
+            return mid;
+        }
+        else if(nums[mid] < nums[mid+1]) low = mid+1;
+        else high = mid-1;
+       }
+       return low;
+    }
+};
