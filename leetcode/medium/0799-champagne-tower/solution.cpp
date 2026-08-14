7595e903cc1985a6acb7ff0f44cda3e5a83bc682
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-08-14 10:38:28 +0530

    ✅ Champagne Tower | Medium | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-08-14

--- leetcode/medium/0799-champagne-tower/solution.cpp
+++ leetcode/medium/0799-champagne-tower/solution.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     double champagneTower(int poured, int row, int query_glass) {
         if(poured == 0) return 0;
+        if(poured == 1 && row == 0) return 1;
         if(poured == 1 && row > 0) return 0;
-        int total_below = (row+1)*(row+2)/2;
-        int total_above = (row+1)*(row)/2;
-        if(poured >= total_below) return 1;
+        int total_below = ((row+1)*(row+2))/2;
+        int total_above = ((row+1)*(row))/2;
+        if(poured > total_below) return 1;
         if(poured <= total_above) return 0;
+        int rem = poured - (total_above);
         if(query_glass > 0 && query_glass < row){
-            return (double)(1/row);
+            return ((double)rem/row);
         }
-        return (double)(1/row*2);
+        
+        double val = ((double)rem/(row*2));
+        return val;
     }
 };
