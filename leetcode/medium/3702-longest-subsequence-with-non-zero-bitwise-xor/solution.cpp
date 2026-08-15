fe321dadde90e4e0752eb8f6d175a3cee0b4073e
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-08-15 08:03:23 +0530

    ✅ Longest Subsequence With Non-Zero Bitwise XOR | Medium | C++ | Runtime: 0 ms | Memory: 8.1 MB | 2026-08-15

---
+++ leetcode/medium/3702-longest-subsequence-with-non-zero-bitwise-xor/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1 && nums[0] == 0) return 0;
+        if(n == 1) return 1;
+        int val = 0;
+        for(int v: nums){
+            int res = val^v;
+           val = res;
+        }
+        if(val == 0) return n-1;
+        return n;
+    }
+};
+//8 4 2 1
+//0 0 0 1
+//0 0 1 0
+//0 0 1 1
+//0 1 0 0 
