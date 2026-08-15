c4bd9b421ba80b17722e57d374a577840a74dd29
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-08-15 21:08:33 +0530

    ✅ Eliminate Maximum Number of Monsters | Medium | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-08-15

---
+++ leetcode/medium/1921-eliminate-maximum-number-of-monsters/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int n = dist.size();
+        vector<double> time(n);
+        for(int i = 0; i < n ; i++){
+            time[i] = (double)dist[i]/speed[i];
+        }
+        sort(time.begin(),time.end());
+        double curr = 0;
+        int cnt = 0;
+        for(int i = 0; i < n ; i++){
+            if(i == 0){
+                cnt++;
+                curr = 1;
+            }
+            else {
+                if(curr + 1 < time[i]){
+                    curr += 1;
+                    cnt++;
+                }
+                else break;
+            }
+        }
+        return cnt;
+    }
+};
