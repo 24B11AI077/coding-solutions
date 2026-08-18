d9373927e0d30f11a120344e570d21e3671f306d
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-08-18 15:19:50 +0530

    ✅ Permutations II | Medium | C++ | Runtime: 492 ms (5.06%) | Memory: 160.7 MB | 2026-08-18

---
+++ leetcode/medium/0047-permutations-ii/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void generate(vector<int> nums, vector<int> temp, vector<int> visited){
+        if(temp.size() == nums.size()){
+            res.push_back(temp); return;
+        }
+        for(int i = 0; i < nums.size() ; i++){
+            if(visited[i] == 0){
+                visited[i] = 1;
+                temp.push_back(nums[i]);
+                generate(nums,temp,visited);
+                temp.pop_back();
+                visited[i] = 0;
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int> temp;
+        vector<int> visited(nums.size());
+        generate(nums,temp,visited);
+        set < vector<int>> st;
+        for(int i = 0; i < res.size() ; i++){
+            st.insert(res[i]);
+
+        }
+        vector<vector<int>>fin;
+        for(auto val : st){
+            fin.push_back(val);
+        }
+        return fin;
+    }
+};
