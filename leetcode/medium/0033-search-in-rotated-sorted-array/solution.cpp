4b5c425e3cfd9b81d1bde89858fc23589ea1e9ad
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-08-19 11:04:53 +0530

    ✅ Search in Rotated Sorted Array | Medium | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-08-19

--- leetcode/medium/0033-search-in-rotated-sorted-array/solution.cpp
+++ leetcode/medium/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low= 0; int right = nums.size()-1;
-        while(low <= right){
-            int mid = (low+right)/2 ;
-            if(nums[mid] == target) return mid;
-            if(nums[mid] > target && nums[low] > target) low = mid+1;
-            else if(nums[mid] < target && nums[right] < target) right = mid-1;
-            else if(nums[mid] < target) low = mid+1;
-            else if(nums[mid] > target ) right = mid-1;
+        int n = nums.size();
+        int low = 0,  high = n-1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] ==  target) return mid;
+            if(nums[mid] < target){
+                if(nums[low] == target) return low;
+                if(nums[low] < target) high = mid-1;
+                else low = mid+1;
+            }
+            else {
+                if(nums[low] == target) return low;
+                if(nums[low] < target ) high = mid - 1;
+                else low = mid+1;
+            }
         }
         return -1;
     }
