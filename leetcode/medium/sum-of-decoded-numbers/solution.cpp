0b7eb515fba9af2004bf0a657b7da0b1e2cd368a
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-08-30 08:25:59 +0530

    ✅ Q2. Sum of Decoded Numbers | Medium | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-08-30

---
+++ leetcode/medium/sum-of-decoded-numbers/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumDecoded(vector<long long>& nums) {
+        int sum = 0;
+        int mod = 1000000007;
+        for(int i = 0; i < nums.size(); i++){
+            int x = nums[i]%10;
+           long long y = nums[i]/10;
+            string temp = to_string(y);
+            string x1 = temp.substr(0,x);
+             string y1 = temp.substr(x);
+            int val1 = stoll(x1);
+            int val2 = stoll(y1);
+            int res = (long long)pow(val1,val2)%mod;
+            sum += res;
+        }
+        return sum;
+    }
+};
