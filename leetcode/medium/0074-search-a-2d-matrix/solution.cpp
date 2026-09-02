1014886cc2688e1c60e340b92fe5fefa0499cb40
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-09-02 14:22:02 +0530

    ✅ Search a 2D Matrix | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 13.2 MB | 2026-09-02

--- leetcode/medium/0074-search-a-2d-matrix/solution.cpp
+++ leetcode/medium/0074-search-a-2d-matrix/solution.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(),
-			cols = matrix[0].size(),
-            row = 0, col = cols - 1;
-			
-        while (row < rows && col > -1) {
-            int cur = matrix[row][col];
-            if (cur == target) return true;
-            if (target > cur) row++;
-            else col--;
-        }
         
+       int m = matrix.size();
+       int n = matrix[0].size();
+
+       int low = 0;
+       int high = n * m - 1;
+
+       while(low <= high){
+        int mid = low + (high - low)/2;
+
+        int row = mid / n;
+        int col = mid % n;
+
+        if(matrix[row][col] == target)
+             return true;
+
+        else if(matrix[row][col] < target)
+            low = mid + 1;
+        
+        else
+           high = mid - 1;
+       }           
         return false;
     }
 };
