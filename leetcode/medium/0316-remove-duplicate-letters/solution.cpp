519ed51d4ef3621405a4fbf680abf77992460177
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-09-16 11:56:27 +0530

    ✅ Remove Duplicate Letters | Medium | C++ | Runtime: 3 ms (15.62%) | Memory: 9.8 MB | 2026-09-16

---
+++ leetcode/medium/0316-remove-duplicate-letters/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char,int> freq;
+        for(char val : s){
+            freq[val]+=1;
+        }
+        stack<char> st;
+        unordered_map<char,bool> checker;
+        unordered_map<char,int> temp;
+        for(char ch : s){
+            while(!st.empty() && ch < st.top() && freq[st.top()] > 1 && !checker[ch]){
+                freq[st.top()] -= 1;
+                
+                checker[st.top()] = false;
+                st.pop();
+            }
+            if(checker[ch] == false) {
+                st.push(ch);
+                checker[ch] = true;
+            }
+            else {
+               freq[ch] -= 1;
+            }
+        }
+        string res = "";
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
