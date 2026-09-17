e33b0b18a0b6c73deb37b295e377ce316b3a19e6
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-09-17 10:10:03 +0530

    ✅ Matrix Block Sum | Medium | C++ | Runtime: 10 ms (23.74%) | Memory: 13.9 MB | 2026-09-17

--- leetcode/medium/1314-matrix-block-sum/solution.cpp
+++ leetcode/medium/1314-matrix-block-sum/solution.cpp
@@ -3,15 +3,15 @@ public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int rows = mat.size();
         int cols = mat[0].size();
-        vector<vector<int>> prefix = mat;
-        // vector<vector<int>> prefix(rows,vector<int> (cols,0));
-        // for(int i = 0; i  < rows ; i++){
-        //     int sum = 0;
-        //     for(int j = 0; j < cols; j++){
-        //         sum += mat[i][j];
-        //         prefix[i][j] = sum;
-        //     }
-        // }
+        // vector<vector<int>> prefix = mat;
+        vector<vector<int>> prefix(rows,vector<int> (cols,0));
+        for(int i = 0; i  < rows ; i++){
+            int sum = 0;
+            for(int j = 0; j < cols; j++){
+                sum += mat[i][j];
+                prefix[i][j] = sum;
+            }
+        }
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 mat[i][j] = 0;
@@ -21,13 +21,12 @@ public:
                 if(lc < 0) lc = 0; // 0 
                 if(ur >= rows) ur = rows-1; // 1
                 if(uc >= cols) uc = cols-1; // 1
-                while(lr <= ur&&lc <= uc){
-                    mat[i][j] += prefix[lr][lc];
-                    lc++;
-                    if(lc > uc){
-                        lc = j-k;
-                        if(lc < 0) lc = 0;
-                        lr++;
+                for(int k = lr ; k <= ur; k++){
+                    if(lc == 0){
+                        mat[i][j] += prefix[k][uc];
+                    }
+                    else {
+                        mat[i][j] += prefix[k][uc]-prefix[k][lc-1];
                     }
                 }
             }
@@ -35,9 +34,10 @@ public:
         return mat;
     }
 };
-// (0,0) , -1 1 , -1 1, 0 1 , 0 1
+// (0,0) , -1 1 , -1 1, 0 1 , 1 2
 
 /*
-   1 2 3
-   4 5 6 
+   1 2 3  1 3 6
+   4 5 6  4 9 15
+   7 8 9  7 15 24
 */
