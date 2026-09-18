7d9a19d2c0dec7b4d60be85dbac133fcb828b8fb
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-09-18 19:43:06 +0530

    ✅ Gas Station | Medium | C++ | Runtime: 2 ms (38.91%) | Memory: 112.6 MB | 2026-09-18

---
+++ leetcode/medium/0134-gas-station/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int curr = 0;
+        int idx = 0;
+        int totalGas = 0, totalCost = 0;
+        for(int i = 0; i < gas.size(); i++){
+             curr += gas[i]-cost[i];
+             if(curr < 0 ){
+                curr = 0;
+                idx = i+1;
+             }
+             totalGas += gas[i];
+             totalCost += cost[i];
+        }
+        if(idx >= gas.size() || totalCost > totalGas) return -1;
+        return idx ;
+    }
+};
