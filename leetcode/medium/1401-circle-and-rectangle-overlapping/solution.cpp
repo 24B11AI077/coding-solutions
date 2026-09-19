1b59a3e5553a804dbe438584d8caad5313768275
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-09-19 19:10:01 +0530

    ✅ Circle and Rectangle Overlapping | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 8 MB | 2026-09-19

--- leetcode/medium/1401-circle-and-rectangle-overlapping/solution.cpp
+++ leetcode/medium/1401-circle-and-rectangle-overlapping/solution.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
-        int left =  xCenter - radius;
-        int right = xCenter + radius;
-        int top = yCenter + radius;
-        int bottom = yCenter - radius;
-        if(x1 >= right || x2 <= left || top <= y1 || bottom >= y2 ) return false;
-        return true;
+    bool checkOverlap(int r, int cx, int cy, int x1, int y1, int x2, int y2) {
+        int x = clamp(cx, x1, x2) - cx;
+        int y = clamp(cy, y1, y2) - cy;
+
+        return x * x + y * y <= r * r;
     }
 };
