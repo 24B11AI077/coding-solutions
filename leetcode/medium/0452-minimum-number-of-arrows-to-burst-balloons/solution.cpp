570189df1605e5d81634ed1df184232eb4783a2a
Jayanth Dadisetti <dadisettijayanth7@gmail.com>
2026-09-22 09:52:24 +0530

    ✅ Minimum Number of Arrows to Burst Balloons | Medium | C++ | Runtime: 42 ms (81.25%) | Memory: 93.7 MB | 2026-09-22

---
+++ leetcode/medium/0452-minimum-number-of-arrows-to-burst-balloons/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+       sort(points.begin(), points.end(), [](const vector<int>& x, const vector<int>& y) {
+            return x[1] < y[1];
+        });
+        int count = 1;
+        int curr = points[0][1];
+        for(int i = 1; i < points.size(); i++){
+            if(curr < points[i][0]) {
+                count++;
+                curr = points[i][1];
+            }
+           
+        }
+        return count;
+    }
+};
